abfdc874f7277d8709c9e439adc879fca069c1b5
Your Name <anjxwyc@outlook.com>
2018-04-26 09:52:17 +0800

    never stop learning

---
+++ src/BinarySearch.py
@@ -0,0 +1,14 @@
+#µİ¹é¶ş·Ö²éÕÒ
+
+a=[i for i in range(1,100000000)]
+def search(a,left,right,num):
+    mid = round((left+right)/2)
+    print(mid)
+    if a[mid]>num:
+        return search(a,left,mid,num)
+    elif a[mid]<num:
+        return search(a,mid,right,num)
+    else:
+        return mid
+        
+print(search(a,0,99999999,5))
