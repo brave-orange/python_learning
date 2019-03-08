3f11101312b1ca889336d9affabe9d094898e571
YongCheng-w <anjxwyc@outlook.com>
2019-03-08 17:38:43 +0800

    p2p

--- p2p_talk/server.py
+++ p2p_talk/server.py
@@ -1,93 +1,96 @@
 #!/usr/bin/env python
 # encoding=utf-8
-import socket
-import threading,time
 
-def tcplink(sock, addr):
-    name=""
-    print('Accept new connection from %s:%s...' % addr)
-    for i in range(0, len(person)):
-        print(person[i])
-        if addr == person[i]['addr']:
-            if person[i]['name'] == "":
-                print("send hello")
-                sock.send(b'sys:What is your name?')
-                data = sock.recv(2048).decode('utf-8')
-                person[i]['name'] = data
-                name = data
-            break
-    online_list = "******************online_list********************\n" 
-    for i in range(0, len(person)):        
-        online_list += "%d : name:%s           ip:%s \n" %  (i,person[i]['name'],person[i]['addr'][0])
-    online_list += "\n\n\n input the %+number(%1) to choose somebody to talk \n input '%public' to talk with all of people,\n input '%refresh' to reload online_list \n input '%-1' to quit,input '%-2' to quit talk." 
-    online_list += "*************************************************\n\n\n" 
-    sock.send(online_list.encode('utf-8'))
-    mode = 0    ##代表当前聊天模式 0：功能选择，1：P2P对话模式，2：广播模式
-    target = ""
-    while True:
-        data = sock.recv(2048).decode('utf-8') 
-        if data[0] == '%':
-            c = data[1:]
-            if c == "public":
-                sock.send(b'sys:Ok,you can talk with all of people now.')
-                mode = 2
-            elif c == "refresh":
-                online_list = "******************online_list********************\n" 
-                for i in range(0, len(person)):        
-                    online_list += "%d : name:%s           ip:%s \n" %  (i,person[i]['name'],person[i]['addr'][0])
-                online_list += "\n\n\n input the %+number(%1) to choose somebody to talk \n input '%public' to talk with all of people,\n input '%refresh' to reload online_list \n input %-1 to quit." 
-                online_list += "*************************************************\n\n\n" 
-                sock.send(online_list.encode('utf-8'))
-            elif c == "-1":
-                sock.send("bye!".encode('utf-8'))
-                sock.close()
-                mode = 0;
-                break
-            elif c == "-2":
-                sock.send("sys:already quit talk.".encode('utf-8'))
-                mode = 0 
-            else:
-                c1 = int(c)
-                target = person[c1]
-                mode = 1
-        else:
-            if mode == 1:
-                str = "%s(%s):%s\n time:%s" % (name,addr,data,time.time())
-                target_sock = target['sock']
-                target_sock.send(str.encode('utf-8'))
-            elif mode == 2:
-                str = "[Radio]%s(%s):%s\n time:%s" % (name,addr,data,time.time())
-                for i in range(0, len(person)):
-                    target_sock = person[i]['sock']
-                    target_sock.send(str.encode('utf-8'))
-            elif mode == 0:
-                online_list = "******************online_list********************\n" 
-                for i in range(0, len(person)):        
-                    online_list += "%d : name:%s           ip:%s \n" %  (i,person[i]['name'],person[i]['addr'][0])
-                online_list += "\n\n\n input the %+number(%1) to choose somebody to talk \n input '%public' to talk with all of people,\n input '%refresh' to reload online_list \n input %-1 to quit." 
-                online_list += "*************************************************\n\n\n" 
-                sock.send(online_list.encode('utf-8'))
-                
-                
+import socket
+import threading,time,fcntl, struct,os,time
+from urllib2 import urlopen
+
+class Iptable(object):
+    def headers(self,str):        #解析数据包
+        header_content = str.split('\r\n\r\n', 1)[0].split('\r\n')[0:]
+        result = {}
+        for line in header_content:
+            k, v = line.split(':')
+            result[quote(k)] = quote(v)
+        return result
+    def __init__(self):
+        self.iptables = set()
+        self.iptables.add("192.168.31.168")#172.31.0.17
+        #self.myaddr = urlopen('http://ip.42.pl/raw').read()#获取本机ip
+        self.myaddr = socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s', "eth0"[:15]))[20:24])
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print "p2p创世节点初始化完成"
+
+        self.send_radio()     #发送广播包得到iptables
         
-    print(person)
+
+    def addip(self,ips):
+        self.iptables = self.iptables | ips #合并现有ip列表
+
+    def send_radio(self):
+        print "开始发送广播"
+        for ip in self.iptables:
+            print "发送给"+ip
+            if ip == self.myaddr:   #不能发送给自己
+                print "暂无其他节点"
+                continue
+            request = "Content-Type:radio\r\n" #表示是广播请求
+            request += "Method:getIptables\r\n" #表示目的是得到IP列表
+            request += "From:"+self.myaddr+"\r\n" #来源ip
+            request += "Referer:"+ip+"\r\n"  #目标ip
+            request += "Port:6666\r\n\r\n"   #端口号
+            print request
+            self.socket.connect((ip,6666)) 
+            self.socket.send(request)
+            print "发送一条广播"
+
+    def getIptables(self,port,ip):  #发送现有的IP列表
+        print "返回iptables至请求的机器"
+        self.socket.connect((ip,port))
+        self.iptables.add(ip)
+        response = "Content-Type:response\r\n" #表示是对点请求
+        response += "Method:refreshIptables\r\n" #表示目的是得到IP列表
+        response += "From:"+self.myaddr+"\r\n" #来源ip
+        response += "Port:6666\r\n\r\n"   #端口号
+        iptables_str = "["
+        for ip in self.iptables:
+            iptables_str += ip + ','
+        iptables_str = iptables_str.strip(',');
+        iptables_str += "]"
+        response += "Iptables:"+iptables_str+"\r\n\r\n"   #端口号
+        self.socket.send(response)
+
+    def refreshIptables(self,header):  #获取更新iptables
+        #self.addip(ips)
+        print "更新现有的iptables"
+        print "网内ip已更新："+header
+
+    def recv(self,sock,addr):   #处理接收到的请求
+        h = sock.recv(1024).decode('utf-8')
+        headers = self.headers(h)
+        if headers["Content-Type"] == "radio":
+            print "收到广播"
+            eval("self."+headers["Method"])(headers["Port"],headers["From"])   #执行操作
+        if headers["Content-Type"] == "response":    #返回数据
+            print "收到回复"
+            eval("self."+headers["Method"])(header)   #执行操作
 
 
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-person = [] #存储来的人
-
 s.bind(('0.0.0.0',6666))
 s.listen(10)
-print('Waiting for connection...')
 
+iptable = Iptable()
+print 'Waiting for connection...'
 while True:
     # 接受一个新连接:
-    sock, addr = s.accept()
-    person.append({'addr':addr,'sock':sock,'name':''})
-    
+    sock, addr = s.accept()    
+    print "有链接"
     # 创建新线程来处理TCP连接:
-    t = threading.Thread(target=tcplink, args=(sock, addr))
+    t = threading.Thread(target=iptable.recv, args=(sock, addr))
+
     t.start()
+    time.sleep(2)
 
