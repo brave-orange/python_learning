fd698b9c46a4b1b0a0ffcc61981ac616ffc492b4
YongCheng <brave-orange@users.noreply.github.com>
2020-09-24 17:34:41 +0800

    Update test1.py
    动态规划递归

--- test1.py
+++ test1.py
@@ -1,16 +1,24 @@
-#
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+dp = {}
 def find_num(n,nums):
+    global dp
+    if int(nums[0:n] ) > 26:
+        return 0
+    if int(nums) < 10:
+        return 1
+    if nums[n:] in dp.keys():
+        return dp[nums[n:]]
+    a += 1
     if nums[n:] == "":
         num = 0
     else:
         num = int(nums[n:])
-    if n == 2:
-        if int(nums[0:n] ) > 26:
-            return 0
-    if num < 26:
+    if num < 10:
         return 1
-    else:
-        return find_num(1,nums[n:])+find_num(2,nums[n:])
+    dp[nums[n:]] = find_num(1,nums[n:])+find_num(2,nums[n:])
+    return dp[nums[n:]]
 
-
-print(find_num(1,"2222222211126")+find_num(2,"226"))
+str= "222222226"
+print(find_num(1,str)+find_num(2,str))
+print(dp)
