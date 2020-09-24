b3602f340bdecd1cbe5680df68065448b66cf4b6
YongCheng <brave-orange@users.noreply.github.com>
2020-09-24 17:39:45 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 dp = {}
+a = 0
 def find_num(n,nums):
     global dp
+    global a
     if int(nums[0:n] ) > 26:
         return 0
     if int(nums) < 10:
         return 1
-    if nums[n:] in dp.keys():
-        return dp[nums[n:]]
-    a += 1
     if nums[n:] == "":
         num = 0
     else:
         num = int(nums[n:])
     if num < 10:
         return 1
-    dp[nums[n:]] = find_num(1,nums[n:])+find_num(2,nums[n:])
+    if nums[n:] not in dp:
+        dp[nums[n:]] = find_num(1,nums[n:])+find_num(2,nums[n:])
     return dp[nums[n:]]
 
 str= "222222226"
